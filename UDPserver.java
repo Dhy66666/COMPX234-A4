77c81f05a799c7304e54f1e11405e8c53902f911
Dhy66666 <152956642+Dhy66666@users.noreply.github.com>
2025-06-08 14:52:34 +0800

    Update UDPserver.java
    编写方法 handleFileTransmission，实现文件传输的处理，包括一个返回接口的方法。

--- UDPserver.java
+++ UDPserver.java
@@ -38,15 +38,59 @@ public class UDPserver {
                     serverSocket.send(new DatagramPacket(errData, errData.length, clientAddress, clientPort));
                 } else {
                     long fileSize = file.length();
-                    //int dataPort = getRandomPort();
-                    //String okMsg = "OK " + filename + " SIZE " + fileSize + " PORT " + dataPort;
-                    //byte[] okData = okMsg.getBytes();
-                    //serverSocket.send(new DatagramPacket(okData, okData.length, clientAddress, clientPort));
+                    int dataPort = getRandomPort();
+                    String okMsg = "OK " + filename + " SIZE " + fileSize + " PORT " + dataPort;
+                    byte[] okData = okMsg.getBytes();
+                    serverSocket.send(new DatagramPacket(okData, okData.length, clientAddress, clientPort));
 
                     // 启动线程处理文件数据传输
-                    //new Thread(() -> handleFileTransmission(file, filename, clientAddress, dataPort)).start();
+                    new Thread(() -> handleFileTransmission(file, filename, clientAddress, dataPort)).start();
                 }
             }
         }
     }
+
+    private static void handleFileTransmission(File file, String filename, InetAddress clientAddress, int port) {
+        try (RandomAccessFile raf = new RandomAccessFile(file, "r")) {
+            DatagramSocket socket = new DatagramSocket(port);
+            byte[] buffer = new byte[BUFFER_SIZE];
+            System.out.println("Thread started on port " + port);
+
+            while (true) {
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+                String msg = new String(packet.getData(), 0, packet.getLength()).trim();
+                String[] parts = msg.split(" ");
+
+                if (parts[0].equals("FILE") && parts[1].equals(filename)) {
+                    if (parts[2].equals("CLOSE")) {
+                        String closeResp = "FILE " + filename + " CLOSE_OK";
+                        socket.send(new DatagramPacket(closeResp.getBytes(), closeResp.length(), clientAddress, packet.getPort()));
+                        break;
+                    } else if (parts.length >= 8 && parts[2].equals("GET")) {
+                        int start = Integer.parseInt(parts[4]);
+                        int end = Integer.parseInt(parts[6]);
+                        int length = end - start + 1;
+
+                        byte[] fileData = new byte[length];
+                        raf.seek(start);
+                        raf.read(fileData);
+
+                        String base64 = Base64.getEncoder().encodeToString(fileData);
+                        String resp = "FILE " + filename + " OK START " + start + " END " + end + " DATA " + base64;
+                        socket.send(new DatagramPacket(resp.getBytes(), resp.length(), clientAddress, packet.getPort()));
+                    }
+                }
+            }
+
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int getRandomPort() {
+        return DATA_PORT_MIN + new Random().nextInt(DATA_PORT_MAX - DATA_PORT_MIN + 1);
+    }
 }
+
